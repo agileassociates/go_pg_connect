1b834d3bc221d3a2711161d869e9df2e1e16ad63
johny babylon <ztredded@gmail.com>
2016-08-24 19:32:56 -0700

    added POST request http

--- hellohttp.go
+++ hellohttp.go
@@ -3,17 +3,39 @@ package main
 import (
     "fmt"
     "database/sql"
-
 	_ "github.com/lib/pq"
-
+    "encoding/json"
+    "github.com/gorilla/mux"
+    "net/http"
   
 )
 
 
-func main() {
+type User struct {
+  ID int "json:id"
+  Name  string "json:username"
+  Email string "json:email"
+  First string "json:first"
+  Last  string "json:last"
+}
 
-  
-  db, err := sql.Open("postgres", "user=elliottchavis dbname=gohttp sslmode=disable")
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+
+  NewUser := User{}
+  NewUser.Name = r.FormValue("user")
+  NewUser.Email = r.FormValue("email")
+  NewUser.First = r.FormValue("first")
+  NewUser.Last = r.FormValue("last")
+  output, err := json.Marshal(NewUser)
+  fmt.Println(string(output))
+  if err != nil {
+    fmt.Println("Something went wrong!")
+  }
+
+ 
+
+   db, err := sql.Open("postgres", "user=elliottchavis dbname=gohttp sslmode=disable")
   
   err = db.Ping()	
 
@@ -21,12 +43,24 @@ func main() {
     panic(err.Error()) 
   }
 
-  rows, err := db.Query("INSERT INTO users (first_name, last_name, username, email) VALUES ('bob', 'jones', 'bj', 'bj@gmail')")
+
+  rows, err := db.Query("INSERT INTO users (first_name, last_name, username, email) VALUES ('" + NewUser.First + "', '"+ NewUser.Last + "', '" + NewUser.Name +"', '" + NewUser.Email + "')" )
+  
   if err != nil {
     fmt.Println(err)
   }
   fmt.Println(rows)
 
+
+
+}
+
+ func main() {
+
+  gorillaRoute := mux.NewRouter()
+  gorillaRoute.HandleFunc("/api/user/create", CreateUser).Methods("GET")
+  http.Handle("/", gorillaRoute)
+  http.ListenAndServe(":8080", nil)
 }
 
 
